07ac47fdd0e8acd2f4267d9df1a71ff9081de15b
Justin Daniel <justinddaniel@gmail.com>
2018-01-12 04:05:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const currentUser = "narf"
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+
+let shortGreeting = "Welcome, " + currentUser[0] + "!"
